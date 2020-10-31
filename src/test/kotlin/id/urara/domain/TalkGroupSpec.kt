0974259469bcc78249b19b29ce01562fe4a98a9b
urara <oura036@gmail.com>
2020-11-01 01:00:46 +0900

    add SortedSet Test for use as Map key

--- src/test/kotlin/id/urara/domain/TalkGroupSpec.kt
+++ src/test/kotlin/id/urara/domain/TalkGroupSpec.kt
@@ -1,47 +1,32 @@
-/*
- * Copyright (C)2018 - Deny Prasetyo <jasoet87@gmail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.urara.domain
 
-import org.amshove.kluent.`should be equal to`
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.util.*
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertNotEquals
-import kotlin.test.assertTrue
 
 object TalkGroupSpec : Spek({
-    describe("TalkGroup") {
-        describe("if have same members") {
-            val tg1 = TalkGroup("田中", "佐藤", "鈴木")
-            val tg2 = TalkGroup("田中", "佐藤", "鈴木")
+    describe("SortedSet") {
+        val tg1 = sortedSetOf<String>("佐藤", "鈴木", "田中", "", "")
+        val tg2 = sortedSetOf<String>("鈴木", "佐藤", "", "田中", "")
 
+        describe("if there are 2 SortedSet have same members different order") {
             it("method equals() should return true") {
                 assertEquals(tg1, tg2)
             }
-        }
-        describe("if have different members") {
-            val tg1 = TalkGroup("田中", "佐藤", "鈴木")
-            val tg2 = TalkGroup("田中", "佐藤", "渡辺")
-
-            it("method equals() should return false") {
-                assertFalse(tg1.equals(tg2))
-                assertNotEquals(tg1, tg2)
+            it("method hashCode() should return same value") {
+                assertEquals(tg1.hashCode(), tg2.hashCode())
             }
+        }
+        describe("SortedSet as Map key") {
+            val map = hashMapOf<TreeSet<String>, Int>()
+            //
+            assertEquals(null, map[tg1])
+            assertEquals(0, map.getOrDefault(tg1, 0))
+            map[tg1] = map.getOrDefault(tg1, 0) + 1
+            map[tg2] = map.getOrDefault(tg2, 0) + 1
+
+            assertEquals(2, map.getOrDefault(tg1, 0))
         }
     }
 })
